ff33381288051404a945016bf56b44f687926003
Robson Brock <robsonbrock@gmail.com>
2020-02-18 16:49:43 -0300

    [IJOR-50] renomeando bpmns com sufixo para 'ut' - user task ou 'st' - service task e adicionando classes para java delegate

---
+++ src/main/java/br/com/itau/co8/delegate/EfetivarPropostaSeguros.java
@@ -0,0 +1,16 @@
+package br.com.itau.co8.delegate;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EfetivarPropostaSeguros implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
