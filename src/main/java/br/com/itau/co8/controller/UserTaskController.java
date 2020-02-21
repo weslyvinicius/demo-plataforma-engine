844ceea8c5d249459afe8b5132c16e6fb9bb2a32
Wesly Vinicius <wesly.borges@zup.com.br>
2020-02-21 09:47:16 -0300

    Alterando complete para id da userTask

--- src/main/java/br/com/itau/co8/controller/UserTaskController.java
+++ src/main/java/br/com/itau/co8/controller/UserTaskController.java
@@ -1,15 +1,20 @@
 package br.com.itau.co8.controller;
 
-import br.com.itau.co8.dto.RequestCompleteDto;
-import lombok.RequiredArgsConstructor;
+import java.util.HashMap;
+import java.util.Optional;
+
 import org.camunda.bpm.engine.FormService;
 import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Optional;
+import br.com.itau.co8.dto.RequestCompleteDto;
+import lombok.RequiredArgsConstructor;
 
 
 @RestController
@@ -21,11 +26,11 @@ public class UserTaskController {
 
     private final FormService formService;
 
-    @PostMapping(value = "{processInstanceId}/complete")
-    public void completeTask(@PathVariable(value = "processInstanceId") String processInstanceId,
+    @PostMapping(value = "{userTaskId}/complete")
+    public void completeTask(@PathVariable(value = "userTaskId") String userTaskId,
                              @RequestBody(required = false) RequestCompleteDto requestCompleteDTO) {
 
-        Task task = taskService.createTaskQuery().processInstanceId(processInstanceId).singleResult();
+        Task task = taskService.createTaskQuery().taskDefinitionKey("userTaskId").singleResult();
 
         if(Optional.ofNullable(requestCompleteDTO).isPresent()) {
             formService.submitTaskForm(task.getId(), requestCompleteDTO.getFormParam());
