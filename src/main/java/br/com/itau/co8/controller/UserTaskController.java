ed5cf2d8a15ad9a5f9c0adcd794e1c608d2ed66c
Wesly Vinicius <wesly.borges@zup.com.br>
2020-02-26 10:25:07 -0300

    criando service da usetTask

--- src/main/java/br/com/itau/co8/controller/UserTaskController.java
+++ src/main/java/br/com/itau/co8/controller/UserTaskController.java
@@ -1,14 +1,9 @@
 package br.com.itau.co8.controller;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
-import org.camunda.bpm.engine.FormService;
-import org.camunda.bpm.engine.RuntimeService;
-import org.camunda.bpm.engine.TaskService;
-import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
-import org.camunda.bpm.engine.runtime.Execution;
-import org.camunda.bpm.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.itau.co8.dto.RequestCompleteDto;
+import br.com.itau.co8.service.UserTaskService;
 import lombok.RequiredArgsConstructor;
 
 
@@ -25,42 +21,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(onConstructor = @_(@Autowired))
 public class UserTaskController {
 
-    private final TaskService taskService;
-
-    private final FormService formService;
-
-    private final RuntimeService runtimeService;
+   private final UserTaskService userTaskService;
 
     @PostMapping(value = "{userTaskId}/process-instance-id/{processInstanceId}/complete")
     public void completeTask(@PathVariable(value = "userTaskId") String userTaskId,
                              @PathVariable(value = "processInstanceId") String processInstanceId,
                              @RequestBody(required = false) RequestCompleteDto requestCompleteDTO) {
 
-        final Execution execution = runtimeService.createNativeExecutionQuery()
-                .sql(createQuery())
-                .parameter("rootProcessInstanceId", processInstanceId.trim())
-                .parameter("userTaskId", userTaskId)
-                .parameter("ative", Boolean.TRUE.toString().toUpperCase())
-                .singleResult();
+        Map<String, Object> formParametros = Optional.ofNullable(requestCompleteDTO).isPresent() ?
+                requestCompleteDTO.getFormParam() : new HashMap<>();
 
-        final String processInstanceIduserTask = ((ExecutionEntity) execution).getProcessInstanceId();
-
-        Task task = taskService.createTaskQuery().processInstanceId(processInstanceIduserTask).singleResult();
-
-        if(Optional.ofNullable(requestCompleteDTO).isPresent()) {
-            formService.submitTaskForm(task.getId(), requestCompleteDTO.getFormParam());
-        }else {
-            formService.submitTaskForm(task.getId(), new HashMap<>());
-		}
+        userTaskService.userTaskComplite(userTaskId, processInstanceId, formParametros);
 	}
 
-	private String createQuery(){
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(" SELECT * FROM ACT_RU_EXECUTION ");
-        sb.append(" WHERE ROOT_PROC_INST_ID_ = #{rootProcessInstanceId} ");
-        sb.append(" AND ACT_ID_ = #{userTaskId} ");
-        sb.append(" AND IS_ACTIVE_ = #{ative} ");
-        return sb.toString();
-    }
 }
---
+++ src/main/java/br/com/itau/co8/service/UserTaskService.java
@@ -0,0 +1,52 @@
+package br.com.itau.co8.service;
+
+import java.util.Map;
+
+import org.camunda.bpm.engine.FormService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
+import org.camunda.bpm.engine.runtime.Execution;
+import org.camunda.bpm.engine.task.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @_(@Autowired))
+public class UserTaskService {
+
+    private final TaskService taskService;
+
+    private final FormService formService;
+
+    private final RuntimeService runtimeService;
+
+    public void userTaskComplite(final String userTaskId, final String processInstanceId, final Map<String,
+                Object> formParametos){
+
+        final Execution execution = runtimeService.createNativeExecutionQuery()
+                .sql(createQuery())
+                .parameter("rootProcessInstanceId", processInstanceId.trim())
+                .parameter("userTaskId", userTaskId)
+                .parameter("ative", Boolean.TRUE.toString().toUpperCase())
+                .singleResult();
+
+        final String processInstanceIduserTask = ((ExecutionEntity) execution).getProcessInstanceId();
+
+        Task task = taskService.createTaskQuery().processInstanceId(processInstanceIduserTask).singleResult();
+        formService.submitTaskForm(task.getId(), formParametos);
+
+    }
+
+    private String createQuery(){
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(" SELECT * FROM ACT_RU_EXECUTION ");
+        sb.append(" WHERE ROOT_PROC_INST_ID_ = #{rootProcessInstanceId} ");
+        sb.append(" AND ACT_ID_ = #{userTaskId} ");
+        sb.append(" AND IS_ACTIVE_ = #{ative} ");
+        return sb.toString();
+    }
+}
